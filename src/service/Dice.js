c982a612bec040e6fabc8e1c0b2bb1addefcdd64
˜Gabrielle <biela.a09@gmail.com>
2021-06-10 23:54:27 -0300

    Criando json

---
+++ src/service/Dice.js
@@ -0,0 +1,48 @@
+const bobEsponja=[
+{
+  id:1,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+},
+
+{
+  id:2,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+},
+{
+  id:3,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+},
+{
+  id:4,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+},
+{
+  id:5,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+},{
+  id:6,
+  nome:"Bob Esponja",
+  descrição:"teste",
+  img: "https://static1.purebreak.com.br/articles/5/85/10/5/@/317506--bob-esponja-calca-quadrada-e-as-maiore-diapo-2.jpg"
+
+}
+]
+
+
+
+export default bobEsponja;
